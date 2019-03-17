bf997fa4f0436559291152f50fa418d55aae91a8
Sam Cotten <samcotten@gmail.com>
2019-03-17 13:51:54 +0100

    gcloud update path

--- trainer/convnet_gcloud.py
+++ trainer/convnet_gcloud.py
@@ -53,14 +53,14 @@ def main(job_dir, **args):
         test_datagen = ImageDataGenerator(rescale=1./255)
 
         train_generator = train_datagen.flow_from_directory(
-            'data/train',
+            job_dir + 'data/train',
             target_size=(150, 150),
             batch_size=32,
             class_mode='categorical'
         )
 
         validation_generator = test_datagen.flow_from_directory(
-            'data/validation',
+            job_dir + 'data/validation',
             target_size=(150, 150),
             batch_size=32,
             class_mode='categorical'
